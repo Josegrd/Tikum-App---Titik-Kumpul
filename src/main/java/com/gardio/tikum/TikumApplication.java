8aa9bb77fb790c444835c10d54835323ce0b9950
Jose Garda Seta <jose8gard@gmail.com>
2024-12-02 13:55:25 +0700

    initials commit

---
+++ src/main/java/com/gardio/tikum/TikumApplication.java
@@ -0,0 +1,13 @@
+package com.gardio.tikum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gardio/tikum/TikumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gardio.tikum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
